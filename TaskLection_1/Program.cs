60c9fc31086ed39918dc384c585aeb335696e07a
Mila <milks0593@gmail.com>
2023-02-23 03:12:45 +0300

    TaskLection1

--- TaskLection_1/Program.cs
+++ TaskLection_1/Program.cs
@@ -39,9 +39,8 @@
 
 //Вид 4 (что-то возвращают, что-то принимают)
 
-// string Method4(int count, string text);
+// string Method4(int count, string text)
 // {
-//     int i = 0;
 //     string result = String.Empty;
 //     for(int i = 0; i < count; i++)
 //     {
@@ -89,34 +88,34 @@
 
 //Задача с массивом
 
-int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+// int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-      Console.Write($"{array[i]} ");  
-    }
-    Console.WriteLine(); 
-}
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//       Console.Write($"{array[i]} ");  
+//     }
+//     Console.WriteLine(); 
+// }
 
-void SelectionSort(int [] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int MinPosition = i;
+// void SelectionSort(int [] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int MinPosition = i;
 
-       for (int j = i + 1; j < array.Length; j++)
-       {
-        if(array[j] < array[MinPosition]) MinPosition = j;
-       }
-        int temporary = array[i];
-        array[i] = array[MinPosition];
-        array[MinPosition] = temporary;
-    }
-}
+//        for (int j = i + 1; j < array.Length; j++)
+//        {
+//         if(array[j] < array[MinPosition]) MinPosition = j;
+//        }
+//         int temporary = array[i];
+//         array[i] = array[MinPosition];
+//         array[MinPosition] = temporary;
+//     }
+// }
 
-PrintArray(arr);
-SelectionSort(arr);
-PrintArray(arr);
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
